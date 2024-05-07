93dc29fb2a1d985e0f159bc77c89343d12600023
Zhe Su <sonic.suzhe@gmail.com>
2024-05-07 13:27:54 -0500

    add material for lab6, spark word count and sql

---
+++ lab_doc/lab6/word-count/wc.py
@@ -0,0 +1,12 @@
+from pyspark import SparkContext, SparkConf
+
+DATA = "./data/*.txt"
+OUTPUT_DIR = "counts" # name of the folder
+
+def word_count():
+    sc = SparkContext("local","Word count example")
+    textFile = sc.textFile(DATA)
+    counts = textFile.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+    counts.saveAsTextFile(OUTPUT_DIR)
+    print("Number of partitions: ", textFile.getNumPartitions())
+word_count()
